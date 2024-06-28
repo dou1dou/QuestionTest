fd4ca195dc43c5399500b4f905f308a68af247d6
dou1dou <137888682+dou1dou@users.noreply.github.com>
2024-06-28 23:29:12 +0800

    添加了地导出题库

--- app/_utils/QuestionUtil.py
+++ app/_utils/QuestionUtil.py
@@ -1,4 +1,6 @@
 from . import DBUtil
+from djangoProject.settings import BASE_DIR
+import os
 
 connection = None
 cursor = None
@@ -15,6 +17,18 @@ finally:
         connection.close()
     if cursor is not None:
         cursor.close()
+
+if not os.path.exists(str(BASE_DIR) + "/static/files"):
+    os.makedirs(str(BASE_DIR) + "/static/files")
+with open(str(BASE_DIR) + "/static/files/questions.csv", "w", encoding="utf-8") as f:
+    f.write("Objective_question_id,Description,ChoiceA,ChoiceB,ChoiceC,ChoiceD,Answer,"
+            "Knowledge_points,Parse,Difficulty\n")
+    for q in questions:
+        for i in range(len(q)):
+            f.write(str(q[i]))
+            if i != len(q) - 1:
+                f.write(",")
+        f.write("\n")
 
 
 def get_all_questions():
